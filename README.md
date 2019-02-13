fe5ecaac4c7478495094ed3c60ad9256c4cf8cd2
zendegino <parhamtrojan@hotmail.com>
2019-02-13 14:04:30 +0330

    '4.4.2'

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 "# test-repository" 
 "# test-repository" 
 "# test-repository" 
+"# test-repository" 
---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-filepath.FilePath",
+    "file": "plugins/cordova-plugin-filepath/www/FilePath.js",
+    "pluginId": "cordova-plugin-filepath",
+    "clobbers": [
+      "window.FilePath"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "cordova-plugin-filepath": "1.5.1"
+};
+// BOTTOM OF METADATA
+});
